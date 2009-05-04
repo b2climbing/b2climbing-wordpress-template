da33d5d4833fca2d8f33b755068a69a43eacb8c4
Ben Sales <bensales@reevoo.com>
2009-05-04 16:32:25 +0100

    Initial Commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+	<div id="footer">
+		&copy;<?php echo date(Y); ?> <?php bloginfo('name'); ?><br />
+		Powered by <a href="http://wordpress.org/">WordPress</a> using <a href="http://www.fireandknowledge.org/blueprint/">Blueprint</a>.
+
+		<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. -->
+	</div>
+
+</div> <!-- /container -->
+</div>
+		<?php wp_footer(); ?>
+</body>
+</html>
